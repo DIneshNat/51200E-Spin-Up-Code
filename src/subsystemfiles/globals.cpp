45666ccde09ca499bfcaa72eb71670171b334d4c
DineshNathaniel <dineshnathaniel@gmail.com>
2022-08-31 11:07:38 -0500

    Removed excess files

--- src/subsystemfiles/globals.cpp
+++
@@ -1,7 +0,0 @@
-#include "main.h"
-
-//CONTROLLER
-extern pros::Controller controller(pros::E_CONTROLLER_MASTER);
-
-//MOTORS
-pros::Motor Motor1(1, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_COUNTS);
